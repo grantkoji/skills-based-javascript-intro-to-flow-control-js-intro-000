9e4122eec59adb96f0e56376fdaeca9e94b7bcbf
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-04 02:26:05 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,7 +24,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  if (age <= 19 && age >= 13 ? "You are a teenager" : "You are not a teenager)"
+  if (age <= 19 && age >= 13 ? "You are a teenager" : "You are not a teenager")
   }
 
   function switchAge(age) {
